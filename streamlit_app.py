ca154eb80b5fb7a37354c2fc7d284e09fc20aefa
user-snow-rivery <110472349+user-snow-rivery@users.noreply.github.com>
2022-08-03 15:06:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,5 @@ streamlit.text(fruityvice_response)
 # 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
